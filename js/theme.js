ac2006aa952b26a6ae1a97775ead8cd76e93e45a
temujin1893 <temujin1893@gmail.com>
2026-02-08 18:53:39 +0530

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,1 @@
+const t=document.getElementById("darkToggle");if(localStorage.getItem("theme")==="dark")document.body.classList.add("dark");t?.addEventListener("click",()=>{document.body.classList.toggle("dark");localStorage.setItem("theme",document.body.classList.contains("dark")?"dark":"light");});
